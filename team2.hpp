c182c8d984365f5d94487c5807bba811195c7fe3
Tamir Ashwal <144041557+TamirAshwal@users.noreply.github.com>
2026-01-26 23:59:49 +0200

    Add files via upload
    updated team 2 to latest version

--- team2.hpp
+++ team2.hpp
@@ -13,26 +13,54 @@ namespace argos {
 
       void ControlStep() override;
 
-      uint8_t getTeamId() const override { return 2; }
+      uint8_t getTeamId() const override { return 1; }
       private:
       /* state enum*/
-      enum Estate{
-         STATE_EXPLORE,
-         STATE_AVOID_OBSTACLE,
-         STATE_TO_FOOD,
-         STATE_RETURN_TO_BASE
-      };
-      CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings m_Readings;
-      Estate eState;
+         enum Estate{
+            STATE_EXPLORE,
+            STATE_AVOID_OBSTACLE,
+            STATE_TO_FOOD,
+            STATE_RETURN_TO_BASE,
+            //Raz's addition
+            STATE_BLOCK
+         };
+         CCI_ColoredBlobOmnidirectionalCameraSensor::SReadings m_Readings;
+         Estate eState;
       // parameters
-      
+
+      // variables for stuck detection and mechanism
+      CVector2 m_lastStuckPosition;     
+      UInt32 m_stuckTimer;             
+      UInt32 m_unstickStepCounter;      
+      bool m_isUnsticking;             
+      const UInt32 STUCK_THRESHOLD_TIME = 50;  
+      const Real STUCK_THRESHOLD_DIST = 0.02f; 
+      UInt32 m_proximityStuckTimer;
+
+      // variables for random walk and explore state
       UInt32 walkCounter;
       UInt32 currMovementDuration;
 		Real leftWheelSpeed;
 		Real rightWheelSpeed;
+      
       bool hasFood;
-   
-
+      //Raz's addition
+      bool blockMode;
+      CVector2 enemyBasePos;
+      //blocking effort
+      bool enemyBaseKnown;
+      bool followingEnemy;
+      CVector2 enemyWithFoodPos;
+      int foodLostCounter;
+      // return to base variables 
+      bool m_bFacingBase = false;        
+      CRadians m_cReturnLineHeading;
+      // interception specific variables (FROM FILE 1)
+      bool isRobotCW; // Clockwise direction flag for blocking
+      float lastAngleToTarget;
+      float filterdError;
+      float interception_alpha;
+      float interception_K;
 
       // helper functions
       bool isObstacleAhead() const;
@@ -41,15 +69,28 @@ namespace argos {
       void chooseCurrMovement();
       bool isFoodVisible() const;
       CVector2 getBasePosition() const;
+      CVector2 relToAbsPosition(const CVector2 blob) const;
+      void enemyBlocking(CVector2 enemyPos, CVector2 enemyGoal);
+      void turn4defence(CVector2 enemyBasePos);
+      //Raz's addition
       std::vector<CVector2> getFoodPositions() const;
-      void moveToFood2();
+      CRadians angle2TeamAhead() const;
+      //blocking effort
+      std::vector<CVector2> enemyWithFoodPossitions() const;
+      void GoToEnemyBaseAndCamp();
+      void StoreEnemyBasePosition();
+      void followEnemy(const CRadians& relativeAngle);
+      bool isTeammateAhead() const;
+   
       
+
       // state functions
       void randomWalk();
-      void avoidObstacle();
-      void moveToFood();
       void returnToBase();
-      CRadians angle2TeamAhead() const;
+      //Raz's addition
+      void moveToFood2();
+      void avoidObstacle2();
+      void block();
 
    };
 }
